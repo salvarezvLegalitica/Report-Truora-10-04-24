749273d9ed585919680d9a0cd84aa1f9dc869c5f
Simón Álvarez <s.alvarezv@analiticaterritorial.com.co>
2024-04-18 19:51:19 -0500

    add cleaning of times from Legalítica platform

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pandas
 
 
-def limpiar_columnas(consultas: pandas.DataFrame):
+def limpiar_columnas_consultas(consultas: pandas.DataFrame):
     # clean creation_date and update_date columns to datetime format
-    consultas['creation_date'] = pandas.to_datetime(
-        consultas['creation_date'].str[:19]
-    )
-    consultas['update_date'] = pandas.to_datetime(
-        consultas['update_date'].str[:19]
+    limpiar_columna_fecha(consultas, 'creation_date')
+    limpiar_columna_fecha(consultas, 'update_date')
+
+def limpiar_columnas_consultas_en_legalitica(consultas: pandas.DataFrame):
+    limpiar_columna_fecha(consultas, 'creation_date_in_legalitica')
+    limpiar_columna_fecha(consultas, 'response_date')
+
+def limpiar_columna_fecha(consultas: pandas.DataFrame, columna: str):
+    consultas[columna] = pandas.to_datetime(
+        consultas[columna].str[:19]
     )
 
 def ordenar_por_fecha(consultas: pandas.DataFrame):
@@ -31,10 +36,12 @@ def agregar_duplicados(consultas: pandas.DataFrame):
         current_row = consultas.iloc[i]
         
         is_duplicated = (
+            current_row['creation_date'] and
+            previous_row['creation_date'] and
             current_row['type'] == previous_row['type'] and
             (
-                current_row['input_national_id'] == previous_row['input_national_id']
-                or current_row['input_tax_id'] == previous_row['input_tax_id']
+                current_row['input_national_id'] == previous_row['input_national_id'] or
+                current_row['input_tax_id'] == previous_row['input_tax_id']
             ) and
             current_row['creation_date'] - previous_row['creation_date'] < pandas.Timedelta(hours=1)
         )
@@ -44,10 +51,18 @@ def agregar_duplicados(consultas: pandas.DataFrame):
 if __name__ == '__main__':
     consultas = pandas.read_csv('../Truora_all.csv')
     
-    limpiar_columnas(consultas)
+    consultas_en_legalitica = pandas.read_csv('../debida_diligencia_consultas_prod.csv')
+    
+    limpiar_columnas_consultas(consultas)
+    
+    limpiar_columnas_consultas_en_legalitica(consultas_en_legalitica)
     
     ordenar_por_fecha(consultas)
     
     agregar_duplicados(consultas)
     
     consultas.to_csv('../Truora_all_cleaned.csv', index=False)
+    consultas_en_legalitica.to_csv(
+        '../debida_diligencia_consultas_prod_cleaned.csv',
+        index=False
+    )
